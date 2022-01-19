fb78e81d1ae940bef815170ca6ce6867d7b3b4f4
Rick Rackley <93223405+safehaven1017@users.noreply.github.com>
2022-01-19 11:50:12 -0500

    comments on newgame.js and added urls for links

--- public/javascripts/newGame.js
+++ public/javascripts/newGame.js
@@ -1,19 +1,21 @@
 // get id out of URL query parameters
 const id = new URLSearchParams(location.search).get('id');
 
-// display first question & give next question
+// set initial variables: questionNumber to track game progress, DEFAULT_SECONDS to determine length of timer,
+// userAnswers to track answers, startDate to track game duration
 let questionNumber = 0;
 const DEFAULT_SECONDS = 15;
 const userAnswers = [];
 const startDate = new Date();
+
+// create var for modal DOM object so that we can toggle it when game is over
 var myModal = new bootstrap.Modal(document.querySelector(".modal"));
 
-
-
-
+// function shuffles answers so that correct answer doesn't show up in the same location
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
+        // we will now switch the locations of two indices
         var temp = array[i];
         array[i] = array[j];
         array[j] = temp;
@@ -21,8 +23,7 @@ function shuffleArray(array) {
     return array;
 }
 
-
-
+// function responsible for generating question/answer html
 function displayQuestion(question, correct_answer, incorrect_answers) {
     const answers = incorrect_answers;
     answers.push(correct_answer);
@@ -37,21 +38,26 @@ function displayQuestion(question, correct_answer, incorrect_answers) {
     }).join('');
 }
 
+// setting timer default duration
 let time = DEFAULT_SECONDS;
-axios.get(`/api/v1/games/${id}`)
 
+// fetching game from database
+axios.get(`/api/v1/games/${id}`)
     .then(game => {
+        
+        // function used to display different question each time user answers question or timer runs out
         function updateQuestion() {
             if (questionNumber < game.data.questions.length) {
                 let currentQuestion = game.data.questions[questionNumber].question;
                 let currentCorrect = game.data.questions[questionNumber].correct_answer;
                 let currentIncorrect = game.data.questions[questionNumber].incorrect_answers;
                 displayQuestion(currentQuestion, currentCorrect, currentIncorrect)
-            } else {
-                console.log("finished game")
-            }
+            } 
         }
+        
+        // function responsible for triggering events based on time
         function timer() {
+            // if time is 0, push an incorrect answer, trigger click event, and move on to next question
             if (time == 0) {
                 questionNumber++;
                 userAnswers.push(null);
@@ -59,6 +65,7 @@ axios.get(`/api/v1/games/${id}`)
                 updateQuestion();
                 time = DEFAULT_SECONDS;
                 return
+            // stop timer if game is finished
             } else if (questionNumber > game.data.questions.length - 1) {
                 clearInterval(interval);
                 document.querySelector("#timer").innerHTML = 0;
@@ -67,45 +74,59 @@ axios.get(`/api/v1/games/${id}`)
             document.querySelector("#timer").innerHTML = time;
             time--;
         }
+
+        // display first question and start timer.
         updateQuestion();
         let interval = setInterval(() => { timer() }, 1000);
 
+        // event listener for clicking answers
         document.addEventListener("click", (e) => {
             if (e.target.classList.contains("answer")) {
                 questionNumber++;
+                // if game is not finished, move on to the next question
                 if (questionNumber < game.data.questions.length) {
                     updateQuestion();
                     clearInterval(interval);
                     time = DEFAULT_SECONDS;
                     interval = setInterval(() => { timer() }, 1000);
                 }
+                // if all the questions are not answered, push player's answer into the answers array
                 if (questionNumber <= game.data.questions.length) {
                     userAnswers.push(e.target.innerHTML);
                 }
+                // conditions for end of the game
                 if (questionNumber >= game.data.questions.length) {
+                    // clear timer interval
                     clearInterval(interval);
-                    let score = 0;
+                    // calculate game duration
                     const endDate = new Date();
-                    console.log(endDate.getTime());
                     const gameTime = endDate.getTime() - startDate.getTime();
-                    console.log(gameTime);
+                    // calculate score by comparing user answers to question array correct_answers
+                    let score = 0;
                     game.data.questions.forEach((question, index) => {
                         if (question.correct_answer == userAnswers[index]) {
                             score++;
                             console.log(question.correct_answer)
                         }
                     });
+                    // put score on modal, and display modal
                     document.querySelector("#staticBackdropLabel").innerHTML = `score: ${score}/${game.data.questions.length}`;
                     myModal.toggle();
-                    console.log(userAnswers, score)
+                    // link to restart game
                     document.querySelector('.again').addEventListener('click', () => {
                         window.location = `/newGame.html?id=${id}`;
                     })
-
+                    // link to make new game
+                    document.querySelector('.new').addEventListener('click', () => {
+                        window.location = `/gamepage.html`;
+                    })
+                    // link to finish game and return to home page
+                    document.querySelector('.finish').addEventListener('click', () => {
+                        window.location = `/index.html`;
+                    })
                 }
             }
         })
-        // console.log(game.data)
     })
 
 
